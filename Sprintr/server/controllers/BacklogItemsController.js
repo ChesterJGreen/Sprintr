4bbfd6af551b54500911c9b9e54bedd83456496a
cj.green <cj.green@intimetec.com>
2021-11-18 09:46:53 -0700

    deleted a line

--- Sprintr/server/controllers/BacklogItemsController.js
+++ Sprintr/server/controllers/BacklogItemsController.js
@@ -71,7 +71,6 @@ export class BacklogItemsController extends BaseController {
   async destroy(req, res, next) {
     try {
       req.body.creatorId = req.userInfo.id
-
       res.send(await backlogItemsService.destroy(req.params.id, req.userInfo.id))
     } catch (error) {
       next(error, 'could not delete')
